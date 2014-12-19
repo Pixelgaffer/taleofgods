30aaa9f2933c1ba2842a0dda711f6aa317418adb
unknown <nico.gruendel@web.de>
2014-12-19 14:20:14 +0100

    First Commit

---
+++ Assets/MultiplayerManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MultiplayerManager : MonoBehaviour {
+
+	public InputField username;
+	public InputField password;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void Awake() {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	public void LogIn() {
+		LogIn (username.text, password.text);
+	}
+
+	public void LogIn(string username, string password) {
+		Application.LoadLevel ("mainmenu");
+	}
+}
---
+++ Assets/Util.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util : MonoBehaviour {
+
+	public void switchToScene(string scene) {
+		Application.LoadLevel (scene);
+	}
+
+}
